b16bac1ea6186f94020fffb3436700eae5f2315b
ishluxx <ishimweemmanuel033@gmail.com>
2024-02-29 18:15:46 +0200

    add

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { delay } from "@/lib/utils";
-import { Metadata } from 'next'
-import { notFound } from 'next/navigation'
 
 const page = () => {
   return (
-    <div>page a </div>
+    <div>page</div>
   )
 }
 
